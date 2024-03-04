75cfa97783500e48934e7766e56354d6a88f4b11
BillyLangdown <billy.langdown01@gmail.com>
2024-03-04 08:55:58 -0500

    changes to votes and comments - altered votes up and down buttons & comment input sizing.

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,11 +1,13 @@
 
 import { useEffect, useState } from "react"
 import { patchArticleVotesById } from "./Utils/api"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function Votes({article, article_id}){
 
    const [voteCount, setVoteCount] = useState(0)
+   const [isDisabled, setIsDisabled] = useState(false)
 
 
    useEffect(()=>{
@@ -17,25 +19,31 @@ export default function Votes({article, article_id}){
    function handleVoteClickPlus(){
 
     setVoteCount( voteCount + 1)
+    setIsDisabled(true)
     patchArticleVotesById(article_id, 1)
    }
 
    function handleVoteClickMinus(){
 
     setVoteCount( voteCount - 1)
+    setIsDisabled(true)
     patchArticleVotesById(article_id, -1)
        
    }
 
 
    return (
-    <div className="d-flex flex-row align-items-center p-2">
+    <div className="d-flex flex-column align-items-center p-2">
         <div className="" >
             <p className="paragraph-font-size ">Votes: {voteCount}</p>
         </div>
       <div className="d-flex justify-between">
-        <button className="m-1" onClick={handleVoteClickPlus}>+</button>
-        <button className="m-1" onClick={handleVoteClickMinus}>-</button>
+        <button disabled={isDisabled} className="btn btn-primary rounded-circle m-1" onClick={handleVoteClickPlus}>
+        <FontAwesomeIcon icon={faArrowUp} />
+        </button>
+        <button  disabled={isDisabled} className="btn btn-primary rounded-circle m-1" onClick={handleVoteClickMinus}>
+        <FontAwesomeIcon icon={faArrowDown} />
+        </button>
       </div>
       
     </div>
